038da2aa56e6d026f8a59cf585ff4d81bf09661e
Bhaswat <shandilya.bhaswat@gmail.com>
2024-10-01 16:30:34 +0530

    Created a base application and hae given the discription of SpringApplication.run(...) method

---
+++ src/main/java/com/te/ems/EMSApplication.java
@@ -0,0 +1,20 @@
+package com.te.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EMSApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("EMSApplication");
+		SpringApplication.run(EMSApplication.class, args);
+	}
+	 public void Com
+
+
+
+	}
+
